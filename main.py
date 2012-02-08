dc0b929ddb88c0f8d5f9c921925fbe666c965dd7
luiscalero <calflores46@hotmail.com>
2012-02-07 18:47:58 -0600

    comenttario inicia

---
+++ main.py
@@ -0,0 +1,10 @@
+
+from Lib.me import*
+num=int(raw_input("Introduce un numero"))
+print factorial_numero(num)
+numero=int(raw_input("Introduce un numero "))
+print numero_primo(numero)
+c=raw_input("Introduce la cadena ")
+print palindroma(c)
+n=int(raw_input("Introduce un numero"))
+print raiz(n)
